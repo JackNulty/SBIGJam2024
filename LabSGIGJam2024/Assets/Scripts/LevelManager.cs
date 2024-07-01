ad1c48f06302b2490f411882ef074ba3a263e852
JackNulty <93530525+JackNulty@users.noreply.github.com>
2024-07-01 23:57:52 +0100

    added "inventory"

--- LabSGIGJam2024/Assets/Scripts/LevelManager.cs
+++ LabSGIGJam2024/Assets/Scripts/LevelManager.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
 
     public static int score;
     public TextMeshProUGUI scoreText;
+    public Image slot1;
+    public Image slot2;
 
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void Update()
+    {
+        if(PlayerScript.currentWeapon == Weapons.Stick)
+        {
+            slot1.color = Color.red;
+        }
+        else
+        {
+            slot1.color = Color.blue;
+        }
+
+        if (PlayerScript.currentWeapon == Weapons.Nunchuckes)
+        {
+            slot2.color = Color.red;
+        }
+        else
+        {
+            slot2.color= Color.blue;
+        }
     }
 
     // Update is called once per frame
