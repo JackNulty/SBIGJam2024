039299b0ae5ce8c61e8593f7c5a387786c65d529
DarraghMcKernan <dmckernan77@gmail.com>
2024-07-01 17:56:06 +0100

    sound depending on speed added

--- LabSGIGJam2024/Assets/Scripts/PlayerScript.cs
+++ LabSGIGJam2024/Assets/Scripts/PlayerScript.cs
@@ -17,6 +17,7 @@ public class PlayerScript : MonoBehaviour
     public GameObject Nunchuckes;
     public GameObject stick;
     public GameObject player;
+    public GameObject soundCircle;
 
     // Start is called before the first frame update
     void Start()
@@ -32,8 +33,27 @@ public class PlayerScript : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rb.velocity = new Vector2(moveDirection.x * (moveSpeed * 1.5f), moveDirection.y * (moveSpeed * 1.5f));
+            soundCircle.transform.localScale = new Vector2(14, 14);
+        }
+        else if (Input.GetKey(KeyCode.C))
+        {
+            rb.velocity = new Vector2(moveDirection.x * (moveSpeed * 0.5f), moveDirection.y * (moveSpeed * 0.5f));
+            soundCircle.transform.localScale = new Vector2(5, 5);
+        }
+        else
+        {
+            rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+            soundCircle.transform.localScale = new Vector2(9, 9);
+        }
+        if (Input.GetKey(KeyCode.W) == false && Input.GetKey(KeyCode.A) == false && Input.GetKey(KeyCode.S) == false && Input.GetKey(KeyCode.D) == false)
+        {
+            soundCircle.transform.localScale = new Vector2(3, 3);
+        }
     }
+
 
     private void OnEnable()
     {
@@ -85,7 +105,7 @@ public class PlayerScript : MonoBehaviour
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 5f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 3f);
     }
 
 }
